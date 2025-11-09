9db85f392cd8f7066856a475f746238dd1865b3c
Kevin Acuña <94419050+kevinacuna1@users.noreply.github.com>
2025-11-09 01:48:25 -0300

    Habilitar el soporte para subir imágenes

--- wp-content/themes/gymfitness/functions.php
+++ wp-content/themes/gymfitness/functions.php
@@ -1,5 +1,15 @@
 <?php
 
+function gymfitness_setup() {
+    // Soporte para imágenes destacadas
+    add_theme_support('post-thumbnails');
+    // Soporte para títulos dinámicos
+    add_theme_support('title-tag');
+}
+
+add_action('after_setup_theme', 'gymfitness_setup');
+
+// Registrar Menús
 function gymfitness_menus()
 {
     register_nav_menus(array(
@@ -7,8 +17,10 @@ function gymfitness_menus()
     ));
 }
 
+// Hook para los menús
 add_action('init', 'gymfitness_menus');
 
+// Agregar Scripts y Styles
 function gymfitness_scripts_styles()
 {
     wp_enqueue_style('normalize', 'https://necolas.github.io/normalize.css/8.0.1/normalize.css', array(), '8.0.1');
@@ -17,4 +29,5 @@ function gymfitness_scripts_styles()
     wp_enqueue_style('style', get_stylesheet_uri(), array('normalize', 'raleway', 'staatliches'), '1.0.0');
 }
 
+// Hook para los scripts y styles
 add_action('wp_enqueue_scripts', 'gymfitness_scripts_styles');
