46f457dc507d80989409e09b80eb5089bd067a45
Kevin Acuña <94419050+kevinacuna1@users.noreply.github.com>
2025-11-10 22:16:40 -0300

    ¿Cuándo crear un tema y cuando crear un plugin?

---
+++ wp-content/plugins/gymfitness-post-types/gymfitness_post_types.php
@@ -0,0 +1,10 @@
+<?php
+/*
+    Plugin Name: Gym Fitness - Post Types
+    Plugin URI: https://github.com/kevinacuna1/dptpwp-web-gimnasio
+    Description: Añade Post Types al sitio Gym Fitness
+    Version: 1.0.0
+    Author: Kevin Acuña
+    Author URI: https://github.com/kevinacuna1
+    Text Domain: gymfitness
+*/
